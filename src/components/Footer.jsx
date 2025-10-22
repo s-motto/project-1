a2bab5fb6ffdc0e93081d1c4001990ab063f6b0f
s-motto <brinamotto@gmail.com>
2025-10-22 21:53:45 +0200

    Change the footer "Powered by" from Geoapify to OpenRouteService

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>
-        Powered by <a href="https://www.geoapify.com/">Geoapify</a>
+    <footer className="text-center py-4 text-sm text-gray-600">
+      Powered by{' '}
+      <a href="https://openrouteservice.org/" className="text-blue-600 hover:underline">
+        OpenRouteService
+      </a>
+      {' '}e{' '}
+      <a href="https://www.openstreetmap.org/" className="text-blue-600 hover:underline">
+        OpenStreetMap
+      </a>
     </footer>
   )
 }
