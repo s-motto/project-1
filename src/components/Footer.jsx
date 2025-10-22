cdec5791cbea1b985bd849a4b253a4db2d4b0f46
s-motto <brinamotto@gmail.com>
2025-10-22 21:11:27 +0200

    Add Footer component to display attribution

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        Powered by <a href="https://www.geoapify.com/">Geoapify</a>
+    </footer>
+  )
+}
+
+export default Footer
