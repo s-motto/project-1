1da38f11563d3ed0ae981aa9c40cced5e78e8e22
s-motto <brinamotto@gmail.com>
2025-10-23 23:12:07 +0200

    Fixed typing error

--- src/appwrite.js
+++ src/appwrite.js
@@ -4,7 +4,7 @@ const client = new Client();
 
 client
   .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
-  .setProject(import.meta.env.VITE_APPWRITE_PROJECT);
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
 export const account = new Account(client);
 export const databases = new Databases(client);
